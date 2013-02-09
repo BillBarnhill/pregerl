a9cf765abf4cb1b502a3df2046c407718aa3c779
Tom Santero <tsantero@gmail.com>
2013-02-09 12:18:46 -0500

    dat riak_core

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ pregerl
 =======
 
 a pregel implementation in Erlang using riak_core
+
+
+Inspried by the [pregel paper](http://kowshik.github.com/JPregel/pregel_paper.pdf)
+
+Project born on Feb 9, 2013 at [ErlangDC](http://erlangdc.com)
---
+++ src/pregerl_wm_ping.erl
@@ -0,0 +1,11 @@
+-module(pregerl_wm_ping).
+-export([init/1, to_html/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+
+init([]) ->
+    {ok, nostate}.
+
+to_html(ReqData, Context) ->
+    Result = io_lib:format("Result: ~p", [pregerl:ping()]),
+    {"<html><head><title>pregerl</title></head><body>" ++ Result ++ "</body></html>", ReqData, Context}.
